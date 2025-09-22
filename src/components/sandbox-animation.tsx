a42bbb701b85591606e95104afb644219c5c76e5
Bikesh Kumar <bikeshkumar0505@gmail.com>
2025-09-22 17:16:29 +0000

    very good , now give some space to sandbar do not chopped during rotatio

--- src/components/sandbox-animation.tsx
+++ src/components/sandbox-animation.tsx
@@ -19,13 +19,13 @@ const SandboxAnimation = ({ percentage, className }: SandboxAnimationProps) => {
 
   return (
     <div className={cn("w-32 h-48", className)}>
-      <svg width="100%" height="100%" viewBox="0 0 100 150" aria-label="Sandbox animation showing time remaining">
+      <svg width="100%" height="100%" viewBox="0 0 150 150" aria-label="Sandbox animation showing time remaining">
         <defs>
           <clipPath id="top-clip">
-            <path d="M 20 15 H 80 L 50 70 L 20 15 Z" />
+            <path d="M 45 15 H 105 L 75 70 L 45 15 Z" />
           </clipPath>
           <clipPath id="bottom-clip">
-            <path d="M 20 135 H 80 L 50 80 L 20 135 Z" />
+            <path d="M 45 135 H 105 L 75 80 L 45 135 Z" />
           </clipPath>
            <linearGradient id="sandGradient" x1="0" y1="0" x2="0" y2="1">
               <stop offset="0%" stopColor="hsl(var(--primary))" />
@@ -35,7 +35,7 @@ const SandboxAnimation = ({ percentage, className }: SandboxAnimationProps) => {
 
         {/* Hourglass Frame */}
         <path 
-          d="M 15 10 H 85 L 50 75 L 15 10 M 15 140 H 85 L 50 75" 
+          d="M 40 10 H 110 L 75 75 L 40 10 M 40 140 H 110 L 75 75" 
           fill="none" 
           stroke="hsl(var(--foreground) / 0.5)" 
           strokeWidth="4" 
@@ -46,7 +46,7 @@ const SandboxAnimation = ({ percentage, className }: SandboxAnimationProps) => {
         {/* Top Sand (disappearing) */}
         <g clipPath="url(#top-clip)">
           <rect 
-            x="15" 
+            x="40" 
             y={15 + (55 - topSandHeight)}
             width="70" 
             height={topSandHeight} 
@@ -58,7 +58,7 @@ const SandboxAnimation = ({ percentage, className }: SandboxAnimationProps) => {
         {/* Bottom Sand (filling) */}
          <g clipPath="url(#bottom-clip)">
           <rect
-            x="15"
+            x="40"
             y={135 - bottomSandHeight}
             width="70"
             height={bottomSandHeight}
@@ -70,7 +70,7 @@ const SandboxAnimation = ({ percentage, className }: SandboxAnimationProps) => {
          {/* Falling Sand Stream */}
         {percentage > 0 && percentage < 100 && (
           <path
-            d="M 50 72 v 6"
+            d="M 75 72 v 6"
             stroke="url(#sandGradient)"
             strokeWidth="2"
             strokeDasharray="1 3"
